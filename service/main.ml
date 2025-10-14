a92583cf883605e693eacb43ddb4ef4d313b687c
Mark Elvers <mark.elvers@tunbury.org>
2025-10-14 08:27:49 +0000

    Added orithia.caelum.ci.dev

--- service/main.ml
+++ service/main.ml
@@ -151,6 +151,7 @@ let main config mode app capnp_address github_auth day10_pool_size prometheus_co
     let ssh_hosts_with_pools = [
       (`X86_64, "localhost", Current.Pool.create ~label:"day10-x86_64" day10_pool_size);
       (`Aarch64, "ainia.caelum.ci.dev", Current.Pool.create ~label:"day10-aarch64" day10_pool_size);
+      (`Ppc64le, "orithia.caelum.ci.dev", Current.Pool.create ~label:"day10-ppc64le" day10_pool_size);
     ] in
     let day10 = Opam_repo_ci.Day10_build.config ssh_hosts_with_pools in
     let engine = Current.Engine.create ~config (Pipeline.v ~day10 ~app) in
